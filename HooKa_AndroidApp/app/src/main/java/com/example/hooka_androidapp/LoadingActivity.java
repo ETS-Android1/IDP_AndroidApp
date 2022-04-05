461f442314f114898db33a45d42e1673acb46310
Zi Hui <46946696+Heng-Zi-Hui@users.noreply.github.com>
2022-04-05 23:17:21 +0800

    updated loading activity

--- HooKa_AndroidApp/app/src/main/java/com/example/hooka_androidapp/LoadingActivity.java
+++ HooKa_AndroidApp/app/src/main/java/com/example/hooka_androidapp/LoadingActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+
 public class LoadingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        //findViewById(R.id.loadingPanel).setVisibility(View.GONE); /*to stop loading animation*/
     }
 }
