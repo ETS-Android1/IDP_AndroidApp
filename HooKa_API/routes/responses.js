5e1fc74eca0586279d6446da4ed73562f4d15c3c
ZhaoYiiii <66521076+ZhaoYiiii@users.noreply.github.com>
2022-04-07 04:31:28 +0800

    api - /responses/updatePoints

--- HooKa_API/routes/responses.js
+++ HooKa_API/routes/responses.js
@@ -3,7 +3,7 @@ const connection = require("../services/db");
 var express = require('express');
 var router = express.Router();
 
-/* Retrieve user's respond to a particular question. :p */
+/* Retrieve user's response to a particular question. :p */
 router.post('/userQuestionResponse', function(req, res, next) {
 
     let qnId = req.body.qnId
@@ -26,5 +26,47 @@ router.post('/userQuestionResponse', function(req, res, next) {
     })
     
   });
+
+  /* compare response to correct answer and update points acordingly. :p */
+    router.post('/updatePoints', function(req, res, next) {
+
+        let qnId = req.body.qnId
+        let userId = req.body.userId
+        let sessionId = req.body.sessionId
+
+        let sql = `select * from response where qnId = ${qnId} and userId = ${userId} and sessionId = ${sessionId}`
+    
+        connection.query(sql, (err, data, fields) => {
+        if (err) throw err
+        
+            userAnswer = data[0].choice
+
+            sql = `select * from questions where qnId = ${qnId}`
+    
+            connection.query(sql, (err, data, fields) => {
+            if (err) throw err
+            
+                correctAnswer = data[0].answer
+
+                let points = 0
+                if(userAnswer==correctAnswer){
+                    points = 10
+                }
+
+                sql = `update response set points = ${points} where qnId = ${qnId} and userId = ${userId} and sessionId = ${sessionId}`
+    
+                connection.query(sql, (err, data, fields) => {
+                    if (err) throw err
+                    
+                        res.status(200).json({
+                        status: "success",
+                        length: data?.length,
+                        });
+                    })
+
+            })
+        })
+        
+    });
 
   module.exports = router;
